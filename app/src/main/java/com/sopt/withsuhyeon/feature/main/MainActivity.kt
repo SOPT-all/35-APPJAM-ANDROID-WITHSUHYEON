305f285a2f75e044afa00baa78278053166639c3
serioushyeon <zz64446949@gmail.com>
2025-01-08 21:10:31 +0900

    #3 [chore] : MainActivity 패키지 이동

--- app/src/main/java/com/sopt/withsuhyeon/feature/main/MainActivity.kt
+++ app/src/main/java/com/sopt/withsuhyeon/feature/main/MainActivity.kt
@@ -1,17 +1,9 @@
-package com.sopt.withsuhyeon
+package com.sopt.withsuhyeon.feature.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.sopt.withsuhyeon.feature.dummy.DummyScreen
 import com.sopt.withsuhyeon.ui.theme.WithSuhyeonTheme
 import dagger.hilt.android.AndroidEntryPoint
