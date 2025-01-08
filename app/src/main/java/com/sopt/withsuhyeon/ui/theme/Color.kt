f7fe199122fad7a1cd566e953e96482e0f774875
sayyyho <323psh@naver.com>
2025-01-08 22:02:31 +0900

    #4 [setting] : Color 기초세팅

--- app/src/main/java/com/sopt/withsuhyeon/ui/theme/Color.kt
+++ app/src/main/java/com/sopt/withsuhyeon/ui/theme/Color.kt
@@ -2,10 +2,35 @@ package com.sopt.withsuhyeon.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple25 = Color(0xFFFAFAFF)
+val Purple50 = Color(0xFFF2F2FF)
+val Purple100 = Color(0xFFDCDDFF)
+val Purple200 = Color(0xFFC5C6FD)
+val Purple300 = Color(0xFF9899F9)
+val Purple400 = Color(0xFF7072F2)
+val Purple500 = Color(0xFF5053E9)
+val Purple600 = Color(0xFF383BDD)
+val Purple700 = Color(0xFF272ACD)
+val Purple800 = Color(0xFF1B1DB8)
+val Purple900 = Color(0xFF1315A0)
+val Purple950 = Color(0xFF0E1087)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Grey25 = Color(0xFFFCFCFD)
+val Grey50 = Color(0xFFF9FAFB)
+val Grey100 = Color(0xFFF3F4F6)
+val Grey200 = Color(0xFFE5E7EB)
+val Grey300 = Color(0xFFD2D6DB)
+val Grey400 = Color(0xFF9DA4AE)
+val Grey500 = Color(0xFF6C737F)
+val Grey600 = Color(0xFF4D5761)
+val Grey700 = Color(0xFF384250)
+val Grey800 = Color(0xFF1F2A37)
+val Grey900 = Color(0xFF111927)
+val Grey950 = Color(0xFF0D121C)
+
+val Black = Color(0xFF000000)
+val Black50 = Color(0x4D000000)
+val White = Color(0xFFFFFFFF)
+
+val Red01 = Color(0xFFFF5747)
+
