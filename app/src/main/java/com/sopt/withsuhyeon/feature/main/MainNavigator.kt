f76a019537ceef7c31bad62a0902205963e49530
serioushyeon <zz64446949@gmail.com>
2025-01-08 21:14:20 +0900

    #3 [feat] : Main Tab, Navigator 세팅

---
+++ app/src/main/java/com/sopt/withsuhyeon/feature/main/MainNavigator.kt
@@ -0,0 +1,82 @@
+package com.sopt.withsuhyeon.feature.main
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavDestination
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navOptions
+import com.sopt.withsuhyeon.core.navigation.MainTabRoute
+import com.sopt.withsuhyeon.core.navigation.Route
+import com.sopt.withsuhyeon.feature.chat.navigation.navigateChat
+import com.sopt.withsuhyeon.feature.findsuhyeon.navigation.navigateFindSuhyeon
+import com.sopt.withsuhyeon.feature.gallery.navigation.navigateGallery
+import com.sopt.withsuhyeon.feature.home.navigation.navigateHome
+import com.sopt.withsuhyeon.feature.mypage.navigation.navigateMyPage
+import com.sopt.withsuhyeon.feature.onboarding.navigation.navigateOnBoarding
+
+class MainNavigator(
+    val navController: NavHostController,
+) {
+    private val currentDestination: NavDestination?
+        @Composable get() = navController
+            .currentBackStackEntryAsState().value?.destination
+
+    val startDestination = MainTab.HOME.route
+
+    val currentTab: MainTab?
+        @SuppressLint("RestrictedApi") @Composable get() = MainTab.find { tab ->
+            currentDestination?.route == tab::class.qualifiedName
+        }
+
+    fun navigate(tab: MainTab) {
+        val navOptions = navOptions {
+            popUpTo(navController.graph.findStartDestination().id) {
+                saveState = true
+            }
+            launchSingleTop = true
+            restoreState = true
+        }
+
+        when (tab) {
+            MainTab.HOME -> navController.navigateHome(navOptions)
+            MainTab.FINDSUHYEON -> navController.navigateFindSuhyeon(navOptions)
+            MainTab.GALLERY -> navController.navigateGallery(navOptions)
+            MainTab.CHAT -> navController.navigateChat(navOptions)
+            MainTab.MYPAGE -> navController.navigateMyPage(navOptions)
+        }
+    }
+
+    fun navigateOnBoarding() {
+        navController.navigateOnBoarding()
+    }
+
+
+    private fun popBackStack() {
+        navController.popBackStack()
+    }
+
+    fun popBackStackIfNotHome() {
+        if (!isSameCurrentDestination<MainTabRoute.Home>()) {
+            popBackStack()
+        }
+    }
+
+    private inline fun <reified T : Route> isSameCurrentDestination(): Boolean =
+        navController.currentDestination?.route == T::class.qualifiedName
+
+    @Composable
+    fun shouldShowBottomBar() = MainTab.contains {
+        currentDestination?.route == it::class.qualifiedName
+    }
+}
+
+@Composable
+internal fun rememberMainNavigator(
+    navController: NavHostController = rememberNavController(),
+): MainNavigator = remember(navController) {
+    MainNavigator(navController)
+}
