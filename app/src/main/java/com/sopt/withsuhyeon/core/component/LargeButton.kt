8e67677efaf6d69676a83a58d25c50868378afa6
sayyyho <323psh@naver.com>
2025-01-12 17:08:28 +0900

    #8 [chore] : small button Preview 수정

---
+++ app/src/main/java/com/sopt/withsuhyeon/core/component/LargeButton.kt
@@ -0,0 +1,2 @@
+package com.sopt.withsuhyeon.core.component
+
--- app/src/main/java/com/sopt/withsuhyeon/core/component/SmallButton.kt
+++ app/src/main/java/com/sopt/withsuhyeon/core/component/SmallButton.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -75,6 +76,7 @@ fun SmallButtonPreview(
     WithSuhyeonTheme {
         Column(
             verticalArrangement = Arrangement.spacedBy(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = modifier
                 .background(White)
                 .fillMaxWidth()
