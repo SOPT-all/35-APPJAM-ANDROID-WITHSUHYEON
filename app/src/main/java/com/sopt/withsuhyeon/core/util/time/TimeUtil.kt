eca7d643b5f42c57b4eb5d45c6db54d5ced21121
yskim6772 <yskim6772@gmail.com>
2025-01-13 11:21:55 +0900

    #7 [chore] : TimeUtil 추가

---
+++ app/src/main/java/com/sopt/withsuhyeon/core/util/time/TimeUtil.kt
@@ -0,0 +1,28 @@
+package com.sopt.withsuhyeon.core.util.time
+
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+
+internal val YEAR_RANGE = (1905..2006).toList()
+internal val MONTH_RANGE = (1..12).toList()
+
+internal val HOUR24_RANGE = (0..23).toList()
+internal val HOUR12_RANGE = (1..12).toList()
+internal val MINUTE_RANGE = (0..59).toList()
+
+internal val currentDateTime = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+
+internal val currentDate = currentDateTime.date
+internal val currentYear = currentDateTime.year
+internal val currentMonth = currentDateTime.monthNumber
+internal val currentMinute = currentDateTime.minute
+internal val currentHour = currentDateTime.hour
+
+enum class TimeFormat {
+    HOUR_12, HOUR_24
+}
+
+enum class TimePeriod {
+    AM, PM
+}
