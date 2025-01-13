fdd05e06e277a76cc7407e45c5c5c47d4d24ebfb
yskim6772 <yskim6772@gmail.com>
2025-01-13 11:17:58 +0900

    #7 [fix] : DummyComponent colors 제거

--- app/src/main/java/com/sopt/withsuhyeon/core/component/DummyComponent.kt
+++ app/src/main/java/com/sopt/withsuhyeon/core/component/DummyComponent.kt
@@ -3,10 +3,8 @@ package com.sopt.withsuhyeon.core.component
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,7 +16,6 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DummyTextField(
     value: String,
@@ -33,7 +30,7 @@ fun DummyTextField(
         value = value,
         onValueChange = onValueChange,
         placeholder = { Text(hint) },
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
             .padding(horizontal = 15.dp),
         visualTransformation = if (isPasswordField && !passwordVisible) PasswordVisualTransformation() else VisualTransformation.None,
@@ -43,18 +40,11 @@ fun DummyTextField(
                 Text(
                     text = if (passwordVisible) "hide" else "show",
                     color = Color.White,
-                    modifier = Modifier
+                    modifier = modifier
                         .clickable { passwordVisible = !passwordVisible }
                         .padding(8.dp)
                 )
             }
         } else null,
-        colors = TextFieldDefaults.textFieldColors(
-            containerColor = Color.White,
-            focusedTextColor = Color.White,
-            unfocusedTextColor = Color.White,
-            focusedPlaceholderColor = Color.Gray,
-            unfocusedPlaceholderColor = Color.Gray
-        )
     )
 }
