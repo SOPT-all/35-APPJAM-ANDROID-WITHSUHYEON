0026e5112f4e7122765c7aff257d291a74c62f44
yskim6772 <yskim6772@gmail.com>
2025-01-14 12:34:42 +0900

    #7 [chore] : FindSuhyeonScreen, GalleryScreen 플로팅 버튼 추가

--- app/src/main/java/com/sopt/withsuhyeon/feature/gallery/GalleryScreen.kt
+++ app/src/main/java/com/sopt/withsuhyeon/feature/gallery/GalleryScreen.kt
@@ -1,6 +1,7 @@
 package com.sopt.withsuhyeon.feature.gallery
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,8 +9,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.sopt.withsuhyeon.core.component.floatingbutton.AddGalleryPostButton
 
 @Composable
 fun GalleryRoute(
@@ -24,14 +28,27 @@ fun GalleryRoute(
 private fun GalleryScreen(
     padding: PaddingValues
 ) {
-    Column(
-        modifier = Modifier.fillMaxSize()
-            .padding(padding),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(padding)
     ) {
-        Text(
-            text = "Gallery Screen"
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .padding(padding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Gallery Screen"
+            )
+        }
+
+        AddGalleryPostButton(
+            modifier = Modifier
+                .align(BottomCenter)
+                .padding(bottom = 16.dp),
+            onClick = { }
         )
     }
 }
