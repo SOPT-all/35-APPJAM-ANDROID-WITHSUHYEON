581f5f861b768cdbf436ea2401845e504b1e4715
yskim6772 <yskim6772@gmail.com>
2025-01-14 21:17:40 +0900

    #7 [fix] : Type.kt 수정 - suit bold, semibold, regular 추가

--- app/src/main/java/com/sopt/withsuhyeon/ui/theme/Type.kt
+++ app/src/main/java/com/sopt/withsuhyeon/ui/theme/Type.kt
@@ -6,17 +6,12 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.sp
 import com.sopt.withsuhyeon.R
 
-
-val withSuhyeonFont = FontFamily(Font(R.font.suit))
-
-val lineHeightStyle = LineHeightStyle(
-    alignment = LineHeightStyle.Alignment.Center,
-    trim = LineHeightStyle.Trim.None
-)
+val withSuhyeonFontBold = FontFamily(Font(R.font.suit_bold))
+val withSuhyeonFontSemiBold = FontFamily(Font(R.font.suit_semibold))
+val withSuhyeonFontRegular = FontFamily(Font(R.font.suit_regular))
 
 @Immutable
 data class WithSuhyeonTypography(
@@ -54,214 +49,184 @@ data class WithSuhyeonTypography(
 
 val defaultWithSuhyeonTypography = WithSuhyeonTypography(
     heading01_B = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontBold,
         fontSize = 32.sp,
         fontWeight = FontWeight(700),
-        lineHeight = 44.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 44.sp
     ),
     heading01_SB = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontSemiBold,
         fontSize = 32.sp,
         fontWeight = FontWeight(600),
-        lineHeight = 44.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 44.sp
     ),
     heading01_R = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontRegular,
         fontSize = 32.sp,
         fontWeight = FontWeight(400),
-        lineHeight = 44.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 44.sp
     ),
     heading02_B = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontBold,
         fontSize = 28.sp,
         fontWeight = FontWeight(700),
-        lineHeight = 40.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 40.sp
     ),
     heading02_SB = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontSemiBold,
         fontSize = 28.sp,
         fontWeight = FontWeight(600),
-        lineHeight = 40.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 40.sp
     ),
     heading02_R = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontRegular,
         fontSize = 28.sp,
         fontWeight = FontWeight(400),
-        lineHeight = 40.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 40.sp
     ),
     heading03_B = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontBold,
         fontSize = 22.sp,
         fontWeight = FontWeight(700),
-        lineHeight = 34.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 34.sp
     ),
     heading03_SB = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontSemiBold,
         fontSize = 22.sp,
         fontWeight = FontWeight(600),
-        lineHeight = 34.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 34.sp
     ),
     heading03_R = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontRegular,
         fontSize = 22.sp,
         fontWeight = FontWeight(400),
-        lineHeight = 34.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 34.sp
     ),
     title01_B = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontBold,
         fontSize = 24.sp,
         fontWeight = FontWeight(700),
-        lineHeight = 36.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 36.sp
     ),
     title01_SB = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontSemiBold,
         fontSize = 24.sp,
         fontWeight = FontWeight(600),
-        lineHeight = 36.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 36.sp
     ),
     title01_R = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontRegular,
         fontSize = 24.sp,
         fontWeight = FontWeight(400),
-        lineHeight = 36.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 36.sp
     ),
     title02_B = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontBold,
         fontSize = 20.sp,
         fontWeight = FontWeight(700),
-        lineHeight = 30.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 30.sp
     ),
     title02_SB = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontSemiBold,
         fontSize = 20.sp,
         fontWeight = FontWeight(600),
-        lineHeight = 30.sp,
-        // lineHeightStyle = lineHeightStyle
+        lineHeight = 30.sp
     ),
     title02_R = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontRegular,
         fontSize = 20.sp,
         fontWeight = FontWeight(400),
-        lineHeight = 30.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 30.sp
     ),
     body01_B = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontBold,
         fontSize = 18.sp,
         fontWeight = FontWeight(700),
-        lineHeight = 28.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 28.sp
     ),
     body01_SB = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontSemiBold,
         fontSize = 18.sp,
         fontWeight = FontWeight(600),
-        lineHeight = 28.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 28.sp
     ),
     body01_R = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontRegular,
         fontSize = 18.sp,
         fontWeight = FontWeight(400),
-        lineHeight = 28.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 28.sp
     ),
     body02_B = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontBold,
         fontSize = 16.sp,
         fontWeight = FontWeight(700),
-        lineHeight = 24.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 24.sp
     ),
     body02_SB = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontSemiBold,
         fontSize = 16.sp,
         fontWeight = FontWeight(600),
-        lineHeight = 24.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 24.sp
     ),
     body02_R = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontRegular,
         fontSize = 16.sp,
         fontWeight = FontWeight(400),
-        lineHeight = 24.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 24.sp
     ),
     body03_B = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontBold,
         fontSize = 14.sp,
         fontWeight = FontWeight(700),
-        lineHeight = 22.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 22.sp
     ),
     body03_SB = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontSemiBold,
         fontSize = 14.sp,
         fontWeight = FontWeight(600),
-        lineHeight = 22.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 22.sp
     ),
     body03_R = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontRegular,
         fontSize = 14.sp,
         fontWeight = FontWeight(400),
-        lineHeight = 22.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 22.sp
     ),
     caption01_B = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontBold,
         fontSize = 12.sp,
         fontWeight = FontWeight(700),
-        lineHeight = 18.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 18.sp
     ),
     caption01_SB = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontSemiBold,
         fontSize = 12.sp,
         fontWeight = FontWeight(600),
-        lineHeight = 18.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 18.sp
     ),
     caption01_R = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontRegular,
         fontSize = 12.sp,
         fontWeight = FontWeight(400),
-        lineHeight = 18.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 18.sp
     ),
     caption02_R = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontRegular,
         fontSize = 10.sp,
         fontWeight = FontWeight(400),
-        lineHeight = 16.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 16.sp
     ),
     caption02_SB = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontSemiBold,
         fontSize = 10.sp,
         fontWeight = FontWeight(600),
-        lineHeight = 16.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 16.sp
     ),
     caption02_B = TextStyle(
-        fontFamily = withSuhyeonFont,
+        fontFamily = withSuhyeonFontBold,
         fontSize = 10.sp,
         fontWeight = FontWeight(700),
-        lineHeight = 16.sp,
-        lineHeightStyle = lineHeightStyle
+        lineHeight = 16.sp
     )
 )
 
