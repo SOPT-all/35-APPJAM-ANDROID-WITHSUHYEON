49bad3ba719ac3315d62cacf21180d65e0736625
serioushyeon <zz64446949@gmail.com>
2025-01-15 14:12:45 +0900

    #14 [add] : 멀티 칩 요소 상수화

--- app/src/main/java/com/sopt/withsuhyeon/core/util/KeyStorage.kt
+++ app/src/main/java/com/sopt/withsuhyeon/core/util/KeyStorage.kt
@@ -2,4 +2,10 @@ package com.sopt.withsuhyeon.core.util
 
 object KeyStorage {
     const val LONG_TEXTFIELD_MAX_LENGTH = 200
+    const val SHORT_TEXTFIELD_MAX_LENGTH = 30
+
+    //수현이 찾기 - MultiSelectChip
+    const val TAKE_A_PHOTO = "사진 촬영"
+    const val VIDEO_CALL = "영상 통화"
+    const val PHONE_CALL = "전화 통화"
 }
