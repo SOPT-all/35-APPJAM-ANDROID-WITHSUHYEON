dae883b1c99887c3c5954548600d4f3fdf71ef3e
yskim6772 <yskim6772@gmail.com>
2025-01-15 17:54:16 +0900

    #12 [fix] : MainScreen 상단 padding 조정

--- app/src/main/java/com/sopt/withsuhyeon/feature/main/MainScreen.kt
+++ app/src/main/java/com/sopt/withsuhyeon/feature/main/MainScreen.kt
@@ -1,5 +1,8 @@
 package com.sopt.withsuhyeon.feature.main
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -7,6 +10,8 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
 import com.sopt.withsuhyeon.feature.main.component.MainBottomBar
 import com.sopt.withsuhyeon.feature.main.component.MainNavHost
 import kotlinx.collections.immutable.toPersistentList
@@ -34,7 +39,12 @@ private fun MainScreenContent(
         content = { padding ->
             MainNavHost(
                 navigator = navigator,
-                padding = padding
+                padding = PaddingValues(
+                    start = padding.calculateStartPadding(layoutDirection = LayoutDirection.Ltr),
+                    end = padding.calculateEndPadding(layoutDirection = LayoutDirection.Ltr),
+                    bottom = padding.calculateBottomPadding(),
+                    top = 16.dp
+                )
             )
         },
         bottomBar = {
