1f2aecabfaf798cec3d6db7a3b34e0a70aeecf34
yskim6772 <yskim6772@gmail.com>
2025-01-15 15:05:40 +0900

    #12 [feat] : 갤러리 bottom sheet - CategoryChip 컴포넌트 구현

---
+++ app/src/main/java/com/sopt/withsuhyeon/core/component/chip/CategoryChip.kt
@@ -0,0 +1,56 @@
+package com.sopt.withsuhyeon.core.component.chip
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.sopt.withsuhyeon.core.util.modifier.noRippleClickable
+import com.sopt.withsuhyeon.ui.theme.WithSuhyeonTheme.colors
+import com.sopt.withsuhyeon.ui.theme.WithSuhyeonTheme.typography
+
+@Composable
+fun CategoryChip(
+    text: String,
+    isSelected: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val chipBackgroundColor = if (isSelected) colors.Purple50 else colors.White
+    val chipBorderColor = if (isSelected) colors.Purple100 else colors.Grey100
+    val chipTextColor = if (isSelected) colors.Purple600 else colors.Grey700
+
+    Row(
+        modifier = modifier
+            .border(
+                width = 1.dp,
+                color = chipBorderColor,
+                shape = RoundedCornerShape(37.dp)
+            )
+            .background(color = chipBackgroundColor, shape = RoundedCornerShape(37.dp))
+            .padding(start = 12.dp, end = 16.dp, top = 12.dp, bottom = 12.dp)
+            .noRippleClickable { onClick() },
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(18.dp)
+                .background(color = colors.Grey200)
+        )
+
+        Text(
+            text = text,
+            color = chipTextColor,
+            style = typography.body03_SB
+        )
+    }
+}
