702a30e5c78139c4d7c8f75d02a98919a58e196a
yskim6772 <yskim6772@gmail.com>
2025-01-19 04:08:50 +0900

    #41 [feat] : ChatRoomScreen 상단 구현

--- app/src/main/java/com/sopt/withsuhyeon/feature/chat/ChatRoomScreen.kt
+++ app/src/main/java/com/sopt/withsuhyeon/feature/chat/ChatRoomScreen.kt
@@ -1,9 +1,20 @@
 package com.sopt.withsuhyeon.feature.chat
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.sopt.withsuhyeon.R
+import com.sopt.withsuhyeon.core.component.card.ChatRoomInfoCardItem
+import com.sopt.withsuhyeon.core.component.topbar.SubTopNavBar
+import com.sopt.withsuhyeon.ui.theme.WithSuhyeonTheme
+import com.sopt.withsuhyeon.ui.theme.WithSuhyeonTheme.colors
 
 @Composable
 fun ChatRoomRoute(
@@ -20,5 +31,33 @@ private fun ChatRoomScreen(
     modifier: Modifier = Modifier,
     viewModel: ChatViewModel = hiltViewModel()
 ) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(padding)
+            .background(colors.White)
+    ) {
+        SubTopNavBar(
+            text = "작심이",
+            btnIcon = painterResource(R.drawable.ic_close),
+            isTextVisible = true,
+            isBtnVisible = false,
+            onCloseBtnClicked = {}
+        )
+        ChatRoomInfoCardItem(
+            region = "강남/역삼/삼성",
+            postTitle = "강남역에서 수현이 되어주실 분!",
+            price = 20000
+        )
+    }
+}
 
+@Preview
+@Composable
+private fun ChatRoomScreenPreview() {
+    WithSuhyeonTheme {
+        ChatRoomScreen(
+            padding = PaddingValues()
+        )
+    }
 }
