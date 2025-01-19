b18edf85ea87e517af15e1ed3c733377421b8f18
serioushyeon <zz64446949@gmail.com>
2025-01-20 03:30:23 +0900

    #32 [chore] 수현이 찾기 스트링 추출

--- app/src/main/java/com/sopt/withsuhyeon/feature/findsuhyeon/FindSuhyeonScreen.kt
+++ app/src/main/java/com/sopt/withsuhyeon/feature/findsuhyeon/FindSuhyeonScreen.kt
@@ -18,9 +18,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.BottomEnd
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.sopt.withsuhyeon.R
 import com.sopt.withsuhyeon.core.component.bottomsheet.LocationBottomSheet
 import com.sopt.withsuhyeon.core.component.dropdown.text.TextSelectDropDownWithIcon
 import com.sopt.withsuhyeon.core.component.floatingbutton.AddFindSuhyeonPostButton
@@ -203,7 +205,7 @@ private fun FindSuhyeonScreen(
                 .fillMaxSize()
         ) {
             MainTopNavBar(
-                text = "수현이 찾기",
+                text = stringResource(R.string.find_suhyeon),
             )
             HorizontalDivider(
                 thickness = 1.dp,
@@ -227,7 +229,8 @@ private fun FindSuhyeonScreen(
                 }
             )
             LazyColumn(
-                modifier = Modifier.wrapContentHeight()
+                modifier = Modifier
+                    .wrapContentHeight()
                     .padding(top = 16.dp, start = 16.dp, end = 16.dp),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
