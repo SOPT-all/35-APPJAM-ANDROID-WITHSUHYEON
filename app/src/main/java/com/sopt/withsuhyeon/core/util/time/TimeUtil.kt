618231ed4ded25c013370a8472fbc7962685a34d
serioushyeon <zz64446949@gmail.com>
2025-01-20 00:19:26 +0900

    #32 [feat] : date 확장함수 추가

--- app/src/main/java/com/sopt/withsuhyeon/core/util/time/TimeUtil.kt
+++ app/src/main/java/com/sopt/withsuhyeon/core/util/time/TimeUtil.kt
@@ -1,8 +1,10 @@
 package com.sopt.withsuhyeon.core.util.time
 
 import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import kotlinx.datetime.LocalDate
 
 internal val YEAR_RANGE = (1905..2006).toList()
 internal val MONTH_RANGE = (1..12).toList()
@@ -19,6 +21,31 @@ internal val currentMonth = currentDateTime.monthNumber
 internal val currentMinute = currentDateTime.minute
 internal val currentHour = currentDateTime.hour
 
+fun String.toDate(): LocalDate {
+    val pattern = Regex("(\\d{1,2})월 (\\d{1,2})일.*")
+    val match = pattern.matchEntire(this) ?: throw IllegalArgumentException("Invalid date format")
+    val (month, day) = match.destructured
+    val currentYear = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).year
+    return LocalDate(currentYear, month.toInt(), day.toInt())
+}
+
+fun Int.toTime(amPm: String): Int {
+    val isPm = amPm == "오후"
+    return when {
+        this == 12 && isPm -> 12
+        this == 12 && !isPm -> 0
+        isPm -> this + 12
+        else -> this
+    }
+}
+
+fun LocalDateTime.toFormattedString(): String {
+    val dayOfWeekKorean = dayOfWeek.name.toKoreanDay()
+    val isAfternoon = hour >= 12
+    val amPm = if (isAfternoon) "오후" else "오전"
+    val hourIn12HourFormat = if (hour % 12 == 0) 12 else hour % 12
+    return "${monthNumber}월 ${dayOfMonth}일 ($dayOfWeekKorean) $amPm $hourIn12HourFormat:${minute.toString().padStart(2, '0')}"
+}
 fun String.toKoreanDay(): String {
     return when (this) {
         "MONDAY" -> "월"
