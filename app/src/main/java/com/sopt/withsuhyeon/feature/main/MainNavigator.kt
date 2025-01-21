dd2fdeed220d868e868b53929c09e94506a85b8b
serioushyeon <zz64446949@gmail.com>
2025-01-22 08:06:03 +0900

    #55 [chore] 네비게이션 에러 수정

--- app/src/main/java/com/sopt/withsuhyeon/feature/main/MainNavigator.kt
+++ app/src/main/java/com/sopt/withsuhyeon/feature/main/MainNavigator.kt
@@ -114,7 +114,7 @@ class MainNavigator(
     }
 
     fun navigateToFindSuhyeon(navOptions: NavOptions? = null) {
-        navController.navigateToHome(
+        navController.navigateToFindSuhyeon(
             navOptions ?: navOptions {
                 popUpTo(navController.graph.findStartDestination().id) {
                     inclusive = true
