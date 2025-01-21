298f273c3331a64852af4c92980c0a3017f7f9c1
serioushyeon <zz64446949@gmail.com>
2025-01-22 00:18:43 +0900

    #48 [chore] 불필요 임포트 제거

--- app/src/main/java/com/sopt/withsuhyeon/feature/home/component/HomeCardItem.kt
+++ app/src/main/java/com/sopt/withsuhyeon/feature/home/component/HomeCardItem.kt
@@ -2,12 +2,10 @@ package com.sopt.withsuhyeon.feature.home.component
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
@@ -16,7 +14,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
