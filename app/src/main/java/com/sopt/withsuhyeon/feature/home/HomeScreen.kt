e7c58a6dc067b0dc7182fc6f6659e527476f357c
serioushyeon <zz64446949@gmail.com>
2025-01-23 01:11:10 +0900

    #65 [del] 불필요 임포느 제거

--- app/src/main/java/com/sopt/withsuhyeon/feature/home/HomeScreen.kt
+++ app/src/main/java/com/sopt/withsuhyeon/feature/home/HomeScreen.kt
@@ -44,7 +44,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.sopt.withsuhyeon.core.component.bottomsheet.BlockBottomSheet
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.airbnb.lottie.compose.LottieAnimation
--- app/src/main/java/com/sopt/withsuhyeon/feature/home/state/HomeState.kt
+++ app/src/main/java/com/sopt/withsuhyeon/feature/home/state/HomeState.kt
@@ -2,7 +2,6 @@ package com.sopt.withsuhyeon.feature.home.state
 
 import com.sopt.withsuhyeon.R
 import com.sopt.withsuhyeon.domain.entity.HomeModel
-import com.sopt.withsuhyeon.domain.entity.PostItemModel
 
 data class HomeState(
     val homeData: HomeModel = HomeModel(
@@ -11,8 +10,6 @@ data class HomeState(
         homePosts = emptyList()
     ),
     val count: Int = 0,
-    //val countTarget: Int= 4357,
     val isRefreshing: Boolean = false,
-    //val postList: List<PostItemModel> = emptyList(),
     val lottieResId: Int = R.raw.home,
 )
